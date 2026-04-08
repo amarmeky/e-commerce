28b4f0769ffbed78daeada53b2eaea97de354e5f
Amar Meky <amarmeky7@gmail.com>
2026-04-08 18:21:36 +0200

    add dashboard admin

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function redirect(){
-        
+        if(Auth::user()->role=='admin'){
+            return view('Admin.home');
+        }else{
+            return view('User.home');
+        }
     }
 }
